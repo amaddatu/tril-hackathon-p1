3a4dff1095964b9a2ba8ba98f495ca2b01ecc9c0
Anthony <amaddatu@gmail.com>
2017-11-12 20:44:14 -0500

    Add new dashboard for the home page.
    This adds a new dashboard for the home page.

--- src/subcomponents/home.js
+++ src/subcomponents/home.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Home extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			logged_in: props.logged_in
+			, name: props.name
+			, role: props.role
+		};
+	}
+	componentWillReceiveProps(nextProps){
+		this.setState({
+			logged_in: nextProps.logged_in
+			, name: nextProps.name
+			, role: nextProps.role
+		});
+	}
   render() {
+  	var homeDash = null;
+  	if(this.state.logged_in){
+  		homeDash = <div className="dashboard">
+  		<h1>Dashboard</h1>
+  		<h2>Hello {this.state.name} ({this.state.role})!</h2>
+  		</div>;
+  	}
+  	else{
+  		homeDash = <h1>Home</h1>;
+  	}
     return (
       <div className="Home">
-        Home
+        {homeDash}
       </div>
     );
   }
